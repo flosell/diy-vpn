33f25cf794b551cb00480e36df71f1431a722e01
flosell <florian.sellmayr@gmail.com>
2014-03-28 18:05:03 +0100

    added a script to import the vpn config into the client

---
+++ set_up_vpn_client.sh
@@ -0,0 +1,11 @@
+DIR=$(mktemp -d -t vpn)
+
+vagrant ssh-config > $DIR/vagrant.ssh.config
+cd $DIR
+scp -q -F vagrant.ssh.config -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  "default:/etc/openvpn/keys/florian.tar.gz" .
+tar xfz florian.tar.gz
+open florian.tblk
+echo "====== Pulled current VPN configuration. Please follow the tunnelblick instructions and then hit ENTER in this terminal to finish and clean up ======"
+read
+cd
+rm -rf $DIR
