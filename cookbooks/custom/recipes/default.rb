b0c7079e3f31cd03776d561a37e06c852298bfd3
flosell <florian.sellmayr@gmail.com>
2014-03-28 14:49:59 +0100

    modified recipe to add firewall-settings: wasn't executed because the script didn't end with .sh

--- cookbooks/custom/recipes/default.rb
+++ cookbooks/custom/recipes/default.rb
@@ -1,4 +1,4 @@
-file "/etc/openvpn/server.up.d/firewall_settings" do
+file "/etc/openvpn/server.up.d/firewall_settings.sh" do
   content "echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
            sysctl -w net.ipv4.ip_forward=1
            iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE"
